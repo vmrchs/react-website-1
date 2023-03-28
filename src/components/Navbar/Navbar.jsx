130410a253aac79a294a9a714ebffef9163065b7
vmrchs <vmrchs@gmail.com>
2023-03-28 11:06:13 -0300

    First commit on this new project

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+export default function Navbar() {
+  return (
+    <>
+      <nav className="navbar">
+        <div className="navbar-container">
+          <Link to="/" className="navbar-logo">
+            OWT
+          </Link>
+        </div>
+      </nav>
+    </>
+  );
+}
